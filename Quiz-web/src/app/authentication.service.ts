a1326c35ccd184e177b1aead139d831412de5c2c
tnsts <t.steblevets@gmail.com>
2020-04-18 18:55:41 +0300

    URL Fix

--- Quiz-web/src/app/authentication.service.ts
+++ Quiz-web/src/app/authentication.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@angular/core';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
- import { Observable, of } from 'rxjs';
-// import { catchError, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 
 import { User } from './models/user';
+import {url} from "../environments/environment.prod";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
-  private url = '/api/v1';
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
@@ -19,26 +18,26 @@ export class AuthenticationService {
   constructor(private http: HttpClient) { }
 
   login(user: User): Observable<User>{
-        return this.http.post<User>(`${this.url}/login`, user, this.httpOptions);
+        return this.http.post<User>(`${url}/login`, user, this.httpOptions);
     }
 
   register(user: User) {
-        return this.http.post<User>(`${this.url}/registration`, user, this.httpOptions);
+        return this.http.post<User>(`${url}/registration`, user, this.httpOptions);
     }
 
   confirmMail (token: string) {
-    return this.http.get<boolean>(`${this.url}/registration/${token}`);
+    return this.http.get<boolean>(`${url}/registration/${token}`);
   }
 
   resetPass(email: string){
-      return this.http.post<string>(`${this.url}/pass-recovery`, email, this.httpOptions);
+      return this.http.post<string>(`${url}/pass-recovery`, email, this.httpOptions);
   }
 
   confirmResetPass (token: string) {
-    return this.http.get<boolean>(`${this.url}/pass-recovery/${token}`);
+    return this.http.get<boolean>(`${url}/pass-recovery/${token}`);
   }
 
   createNewPass(token: string, newpass: string){
-    return this.http.put<string>(`${this.url}/pass-recovery/${token}`, newpass, this.httpOptions);
+    return this.http.put<string>(`${url}/pass-recovery/${token}`, newpass, this.httpOptions);
   }
 }
