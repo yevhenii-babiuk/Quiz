9adf19b6ff881bc1639aac86534863f99a0a19d8
AndrikB <43090897+AndrikB@users.noreply.github.com>
2020-04-22 17:31:00 +0300

    Dev page for create quiz. fixed bugs (#45)
    * refactor dir
    
    * refactor code
    
    * change dao
    
    * create models, hide footer, refactor code
    
    * add component for creating/updating quiz. add controllers
    
    * try to develop editQuiz page
    
    * fixed bug with QuestionType enum in pages
    
    * create viewQuiz page. fixed bugs
    
    * fixed checkstyle
    
    Co-authored-by: alexandrakmet <alexandrakmett@gmail.com>

---
+++ Quiz-server/src/main/java/com/qucat/quiz/controllers/QuizController.java
@@ -0,0 +1,29 @@
+package com.qucat.quiz.controllers;
+
+import com.qucat.quiz.repositories.entities.Quiz;
+import com.qucat.quiz.services.QuizService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/quiz")
+public class QuizController {
+
+    @Autowired
+    private QuizService quizService;
+
+    @PostMapping
+    public boolean addQuiz(@RequestBody Quiz quiz) {
+        return quizService.createQuiz(quiz);
+    }
+
+    @PutMapping
+    public void updateQuiz(@RequestBody Quiz quiz) {
+        quizService.updateQuiz(quiz);
+    }
+
+    @GetMapping("{id}")
+    public Quiz getQuiz(@PathVariable int id) {
+        return quizService.getQuizById(id);
+    }
+}
