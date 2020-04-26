eb133c7995318efa267000c49c6847d59ae52f96
Yevhenii Babiuk <evgeniy0199@gmail.com>
2020-04-26 18:15:44 +0300

    Add 5 graphic

---
+++ Quiz-web/src/app/modules/dashboard/friends-preference/friends-preference.component.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-friends-preference',
+  templateUrl: './friends-preference.component.html',
+  styleUrls: ['./friends-preference.component.css']
+})
+export class FriendsPreferenceComponent implements OnInit {
+  echartsInstance: any;
+  options = {
+    backgroundColor: '#2c343c',
+
+    title: {
+      text: 'Customized Pie',
+      left: 'center',
+      top: 20,
+      textStyle: {
+        color: '#ccc'
+      }
+    },
+
+    tooltip: {
+      trigger: 'item',
+      formatter: '{a} <br/>{b} : {c} ({d}%)'
+    },
+
+    visualMap: {
+      show: false,
+      min: 80,
+      max: 600,
+      inRange: {
+        colorLightness: [0, 1]
+      }
+    },
+    series: [
+      {
+        name: 'Counters',
+        type: 'pie',
+        radius: '55%',
+        center: ['50%', '50%'],
+        data: [
+          { value: 335, name: 'C-1' },
+          { value: 310, name: 'C-2' },
+          { value: 274, name: 'C-3' },
+          { value: 235, name: 'C-4' },
+          { value: 400, name: 'C-5' }
+        ].sort(function (a, b) { return a.value - b.value; }),
+        roseType: 'radius',
+        label: {
+          normal: {
+            textStyle: {
+              color: 'rgba(255, 255, 255, 0.3)'
+            }
+          }
+        },
+        labelLine: {
+          normal: {
+            lineStyle: {
+              color: 'rgba(255, 255, 255, 0.3)'
+            },
+            smooth: 0.2,
+            length: 10,
+            length2: 20
+          }
+        },
+        itemStyle: {
+          normal: {
+            color: '#c23531',
+            shadowBlur: 200,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          }
+        },
+
+        animationType: 'scale',
+        animationEasing: 'elasticOut',
+        animationDelay: function (idx) {
+          return Math.random() * 200;
+        }
+      }
+    ]
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onChartInit(e: any) {
+    this.echartsInstance = e;
+    console.log('on chart init:', e);
+  }
+}
