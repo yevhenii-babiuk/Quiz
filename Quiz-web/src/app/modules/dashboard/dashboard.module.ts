16ad20ba60e779418767a02ae54cbccb7a348219
Yevhenii Babiuk <evgeniy0199@gmail.com>
2020-04-26 15:18:00 +0300

    add second diagram for user

--- Quiz-web/src/app/modules/dashboard/dashboard.module.ts
+++ Quiz-web/src/app/modules/dashboard/dashboard.module.ts
@@ -2,19 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {DashboardComponent} from "./dashboard.component";
 import {NgxEchartsModule} from "ngx-echarts";
-/*import {ChartModule} from 'primeng/chart';*/
+import {ChartModule} from 'primeng/chart';
 import { PlayedProgressComponent } from './played-progress/played-progress.component';
-/*import { PlayedQuizScoreComponent } from './played-quiz-score/played-quiz-score.component';*/
+import { PlayedQuizScoreComponent } from './played-quiz-score/played-quiz-score.component';
 
 @NgModule({
   declarations: [
     DashboardComponent,
     PlayedProgressComponent,
-/*    PlayedQuizScoreComponent*/
+    PlayedQuizScoreComponent
   ],
   imports: [
     NgxEchartsModule,
-/*    ChartModule,*/
+    ChartModule,
     CommonModule
   ]
 })
