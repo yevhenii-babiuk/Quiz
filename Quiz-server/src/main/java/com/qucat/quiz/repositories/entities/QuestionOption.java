1ec20dbb0425a9a656f8130cf671c0a3a73fdec8
AndrikB <blagij00@gmail.com>
2020-04-27 21:13:51 +0300

    add jsonProperty for isCorrect field in QuestionOption

--- Quiz-server/src/main/java/com/qucat/quiz/repositories/entities/QuestionOption.java
+++ Quiz-server/src/main/java/com/qucat/quiz/repositories/entities/QuestionOption.java
@@ -1,5 +1,6 @@
 package com.qucat.quiz.repositories.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -9,6 +10,7 @@ public class QuestionOption {
     private int id;
     private int questionId;
     private String content;
+    @JsonProperty("isCorrect")
     private boolean isCorrect;
     private int sequenceOrder;
     private int imageId;
