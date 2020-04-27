b2c3a9349ac0a731d7e2b9585022bf4f1cbc0897
AndrikB <blagij00@gmail.com>
2020-04-27 20:58:57 +0300

    update url for date filter

--- Quiz-web/src/app/modules/quiz/quizzes/vertical-filter-bar/date-filter/date-filter.component.ts
+++ Quiz-web/src/app/modules/quiz/quizzes/vertical-filter-bar/date-filter/date-filter.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, EventEmitter, Output, ViewEncapsulation } from '@angular/core';
 import { DateFilter } from './date-filter.model';
 import { FBFilterComponent } from '../fb-filter-component.interface';
-
+import {DatePipe} from "@angular/common";
 @Component({
   selector: 'app-range-filter',
   templateUrl: './date-filter.component.html',
@@ -23,10 +23,10 @@ export class DateFilterComponent implements OnInit, FBFilterComponent {
 
   rangeChanged(_event: any): void {
     //let val=(<HTMLInputElement>document.getElementById('min')).value
-    this.model.filterValue[0]=this.model.currMin.toDateString();
-    this.model.filterValue[1]=this.model.currMax.toDateString();
-    if(this.model.filterValue[0] === this.model.min.toDateString() &&
-      this.model.filterValue[1] === this.model.max.toDateString())
+    this.model.filterValue[0]=this.model.currMin.toISOString().slice(0,10);
+    this.model.filterValue[1]=this.model.currMax.toISOString().slice(0,10);
+    if(this.model.filterValue[0] === this.model.min.toISOString().slice(0,10) &&
+      this.model.filterValue[1] === this.model.max.toISOString().slice(0,10))
       this.change.emit(undefined)
     else{
       this.change.emit(this.model.filterValue)
