fe1e08343233bf2414d104dee9ab7c58e9a3df33
AndrikB <blagij00@gmail.com>
2020-04-28 03:32:49 +0300

    clean code. fix bug with sending request to server in quizzes page

--- Quiz-web/src/app/modules/quiz/quizzes/quizzes.component.ts
+++ Quiz-web/src/app/modules/quiz/quizzes/quizzes.component.ts
@@ -1,14 +1,11 @@
 import {Component, OnInit, HostListener} from '@angular/core';
 import {QuizzesService} from "../../core/services/quizzes.service";
 import {Quiz} from "../../core/models/quiz";
-import {Tag} from "../../core/models/tag";
 import {KeywordFilter} from "./vertical-filter-bar/keyword-filter/keyword-filter.model";
 import {CheckboxFilter} from "./vertical-filter-bar/checkbox-filter/checkbox-filter.model";
 import {FBFilter} from "./vertical-filter-bar/fb-filter.interface";
 import {ActivatedRoute, Router} from "@angular/router";
 import {DateFilter} from "./vertical-filter-bar/date-filter/date-filter.model";
-import {Category} from "../../core/models/category";
-/*import {DateFilter} from "./vertical-filter-bar/date-filter/date-filter.model";*/
 
 @Component({
   selector: 'app-quizzes',
@@ -30,12 +27,7 @@ export class QuizzesComponent implements OnInit {
   ) {
   }
 
-  public manufacturers: string[] = ['Apple', 'Dell', 'Acer', 'HP', 'Lenovo', 'Asus', 'Alienware', 'Cooler Master', 'Fujitsu', 'IBM', 'LG Eletronics', 'MSI', 'Nvidia', 'Panasonic', 'Samsung']
-  public conditions: string[] = ['New', 'Refurbished', 'Used']
-  public displaySizes: string[] = ['17 Inches and Above', '16 to 16.9 Inches', '15 to 15.9 Inches', '14 to 14.9 Inches', '13 to 13.9 Inches', '12 to 12.9 Inches', '12 Inches and Under']
-  public ramSize: string[] = ['32GB and Above', '16GB', '12GB', '8GB', '6GB', '4GB', '3GB', '2GB and Under']
-
-  verticalBarFilters: FBFilter[] = [
+   verticalBarFilters: FBFilter[] = [
     new KeywordFilter('quizName', 'Quiz name'),
     new KeywordFilter('authorName', 'Author name'),
     new CheckboxFilter('category', 'Categories', this.categories),
@@ -66,6 +58,7 @@ export class QuizzesComponent implements OnInit {
           if (quizzes.length == 0) {
             return;
           }
+
           this.isWaiting=false;
           this.quizzes = this.quizzes.concat(quizzes);
         },
@@ -105,10 +98,10 @@ export class QuizzesComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    console.log("here1");
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe(_params => {
       console.log(this.router.url.substring(this.router.url.indexOf("?")+1));
       this.quizzes=[];
+      this.isWaiting=false;
       this.params="?";
       if (this.router.url.indexOf("?"))
         this.params+=this.router.url.substring(this.router.url.indexOf("?")+1);
