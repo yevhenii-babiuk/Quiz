1420c7c6703929677da218ad5a401a93c15a3a3e
AndrikB <blagij00@gmail.com>
2020-04-28 20:26:16 +0300

    update getQuizzes controller. get date from front

--- Quiz-server/src/main/java/com/qucat/quiz/controllers/QuizListController.java
+++ Quiz-server/src/main/java/com/qucat/quiz/controllers/QuizListController.java
@@ -3,13 +3,15 @@ package com.qucat.quiz.controllers;
 import com.qucat.quiz.repositories.entities.Quiz;
 import com.qucat.quiz.services.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Timestamp;
 import java.util.Arrays;
-import java.util.Optional;
+import java.util.Date;
 
 @RestController
 @RequestMapping("api/v1/quizzes")
@@ -22,10 +24,14 @@ public class QuizListController {
     public Quiz[] getQuizzes(
             @RequestParam(value = "category", required = false) String[] categories,
             @RequestParam(value = "tag", required = false) String[] tags,
-            @RequestParam(value = "count") int currentCount) {
-        System.out.println(Arrays.toString(categories));
-        System.out.println(Arrays.toString(tags));
-        return quizService.showPage(Optional.of(currentCount), Optional.of(20)).toList().toArray(Quiz[]::new);
+            @RequestParam(value = "count") int currentCount,
+            @RequestParam(value = "quizName") String quizName,
+            @RequestParam(value = "authorName") String authorName,
+            @RequestParam(value = "date")
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)Date[] dates) {
+        Timestamp t1 = null, t2 = null;
+        return quizService.showPage(currentCount, 20,
+                quizName, authorName, Arrays.asList(categories), t1, t2, Arrays.asList(tags)).toList().toArray(Quiz[]::new);
 
     }
 
