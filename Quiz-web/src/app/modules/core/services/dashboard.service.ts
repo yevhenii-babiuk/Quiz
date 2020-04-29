6c77fb2a690f43a70f8cdcd1d4e3c73ff21061bc
Yevhenii Babiuk <evgeniy0199@gmail.com>
2020-04-29 11:51:27 +0300

    Bug with plot CorrectAnswerPercent graph

--- Quiz-web/src/app/modules/core/services/dashboard.service.ts
+++ Quiz-web/src/app/modules/core/services/dashboard.service.ts
@@ -6,6 +6,9 @@ import {catchError} from "rxjs/operators";
 
 import {User} from '../models/user'
 import {url} from "../../../../environments/environment.prod";
+import {SecurityService} from "./security.service";
+import {Statistics} from "../models/statistics";
+
 @Injectable({
   providedIn: 'root'
 })
@@ -15,21 +18,29 @@ export class DashboardService {
     headers: new HttpHeaders({'Content-Type': 'application/json'})
   };
 
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private securityService: SecurityService) {
   }
 
   getTopUsers(): Observable<User[]> {
-    return this.http.get<User[]>(`${url}/dashboard/top`)
+    return this.http.get<User[]>(`${url}/profile/dashboard/top`)
       .pipe(
         catchError(this.handleError<User[]>([]))
       );
   }
 
+  getQuizzesCorrectAnswerPercent(): Observable<Statistics[]> {
+    let id=this.securityService.getCurrentId();
+    return this.http.get<Statistics[]>(`${url}/profile/dashboard/${id}/quizzes/played/percent`)
+      .pipe(
+        catchError(this.handleError<Statistics[]>([]))
+      );
+  }
+
   private handleError<T>(result?: T) {
     return (error: any): Observable<T> => {
-
-      console.error(error); // log to console instead
-
+      console.error(error);
       return of(result as T);
     };
   }
--- Quiz-web/src/app/modules/dashboard/correct-answers-percent/correct-answers-percent.component.ts
+++ Quiz-web/src/app/modules/dashboard/correct-answers-percent/correct-answers-percent.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { graphic } from 'echarts';
+import {DashboardService} from "../../core/services/dashboard.service";
+import {Statistics} from "../../core/models/statistics";
 
 @Component({
   selector: 'app-correct-answers-percent',
@@ -9,24 +11,43 @@ import { graphic } from 'echarts';
 
 export class CorrectAnswersPercentComponent implements OnInit {
 
+  stat: Statistics[] = [];
   options: any;
   detectEventChanges = true;
 
-  constructor() { }
+  constructor(private dashboardService: DashboardService) { }
 
-  ngOnInit() {
-    const dataAxis = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'];
-    const data = [220, 182, 191, 234, 290, 330, 310, 123, 442, 321, 90, 149, 210, 122, 133, 334, 198, 123, 125, 220];
-    const yMax = 500;
+  ngOnInit(): void{
+    this.getDate();
+    console.log(this.stat);
+    this.plotGraph();
+
+  }
+
+  getDate() {
+    this.dashboardService.getQuizzesCorrectAnswerPercent().subscribe(statistics => {
+        console.log(statistics);
+        this.stat = statistics;
+      },
+      err => {
+        console.log(err);
+      });
+  }
+
+  plotGraph(){
+    let dataAxis = [];
+    this.stat.forEach(element=>dataAxis.push(element.name))
+    let data = [];
+    this.stat.forEach(element=>dataAxis.push(element.count))
+    const yMax = 1;
     const dataShadow = [];
 
     for (let i = 0; i < data.length; i++) {
       dataShadow.push(yMax);
     }
-
     this.options = {
       title: {
-        text: 'Check Console for Events'
+        text: 'Percents of correct answers'
       },
       xAxis: {
         data: dataAxis,
@@ -101,6 +122,7 @@ export class CorrectAnswersPercentComponent implements OnInit {
         }
       ]
     };
+
   }
 
   onChartEvent(event: any, type: string) {
