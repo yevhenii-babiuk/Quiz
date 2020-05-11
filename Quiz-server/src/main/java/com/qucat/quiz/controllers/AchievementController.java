d407d52133d3832ebbbc7e229b3e3030d0624c6c
Yevhenii Babiuk <evgeniy0199@gmail.com>
2020-05-11 21:12:45 +0300

    Add AchievementController

---
+++ Quiz-server/src/main/java/com/qucat/quiz/controllers/AchievementController.java
@@ -0,0 +1,45 @@
+package com.qucat.quiz.controllers;
+
+import com.qucat.quiz.repositories.entities.Achievement;
+import com.qucat.quiz.repositories.entities.AchievementCharacteristic;
+import com.qucat.quiz.services.AchievementCharacteristicService;
+import com.qucat.quiz.services.AchievementService;
+import com.qucat.quiz.services.UserAchievementsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/")
+public class AchievementController {
+
+    @Autowired
+    private AchievementService achievementService;
+
+    @Autowired
+    private UserAchievementsService userAchievementsService;
+
+    @Autowired
+    private AchievementCharacteristicService achievementCharacteristicService;
+
+    @GetMapping("/profile/{id}/achievements/")
+    public List<Achievement> getUserAchievement(@PathVariable int id){
+        return userAchievementsService.getAchievementsByUserId(id);
+    }
+
+    @GetMapping("/achievement/characteristics")
+    public List<AchievementCharacteristic> getAchievementCharacteristics(){
+        return achievementCharacteristicService.getAll();
+    }
+
+    @PostMapping("/achievement/create")
+    public boolean createAchievement(@RequestBody Achievement achievement){
+        return achievementService.createAchievement(achievement);
+    }
+}
