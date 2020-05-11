c7f171bbbdca24d1f24cb1622ba8bacad8fd647c
Yevhenii-Babiuk <51458577+Yevhenii-Babiuk@users.noreply.github.com>
2020-05-11 17:57:05 +0300

    Fix connection pool (#78)
    * Add achievements list to user profile
    * Fix connection pool
    * Add achievements front

--- Quiz-web/src/app/app.module.ts
+++ Quiz-web/src/app/app.module.ts
@@ -28,6 +28,8 @@ import {DashboardModule} from "./modules/dashboard/dashboard.module";
 import {DashboardRoutingModule} from "./modules/dashboard/dashboard-routing.module";
 import {PlayQuizModule} from "./modules/play-quiz/play-quiz.module";
 import {PlayQuizRoutingModule} from "./modules/play-quiz/play-quiz-routing.module";
+import {AchievementRoutingModule} from "./modules/achivement/achievement-routing.module";
+import {AchievementModule} from "./modules/achivement/achievement.module";
 
 @NgModule({
   declarations: [
@@ -60,6 +62,8 @@ import {PlayQuizRoutingModule} from "./modules/play-quiz/play-quiz-routing.modul
     DashboardRoutingModule,
     PlayQuizModule,
     PlayQuizRoutingModule,
+    AchievementModule,
+    AchievementRoutingModule
   ],
   providers: [
     {provide:HTTP_INTERCEPTORS, useClass:BasicAuthHtppInterceptorService, multi:true},
