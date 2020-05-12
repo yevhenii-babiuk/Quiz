9045cf78fa837f8a90db8fffba248b658e27ed83
AndrikB <blagij00@gmail.com>
2020-05-12 19:45:47 +0300

    add scheduler

---
+++ Quiz-server/src/main/java/com/qucat/quiz/services/SchedulerService.java
@@ -0,0 +1,22 @@
+package com.qucat.quiz.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+@EnableScheduling
+@Slf4j
+public class SchedulerService {
+
+    @Autowired
+    private AchievementService achievementService;
+
+    @Scheduled(fixedRate = 5000)//todo change time
+    public void reportCurrentTime() {
+        achievementService.updateUserAchievementLists();
+    }
+
+}
