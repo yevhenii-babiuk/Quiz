887573ad425207b2e0b1c621172930d572aa78b1
AndrikB <blagij00@gmail.com>
2020-05-12 19:38:21 +0300

    add getNewUserAchievements method i achievementDao

--- Quiz-server/src/main/java/com/qucat/quiz/services/UserAchievementsService.java
+++ Quiz-server/src/main/java/com/qucat/quiz/services/UserAchievementsService.java
@@ -2,12 +2,13 @@ package com.qucat.quiz.services;
 
 import com.qucat.quiz.repositories.dao.UserAchievementsDao;
 import com.qucat.quiz.repositories.entities.Achievement;
-import com.qucat.quiz.repositories.entities.User;
+import com.qucat.quiz.repositories.entities.UserAchievement;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -16,12 +17,22 @@ public class UserAchievementsService {
     @Autowired
     private UserAchievementsDao userAchievementsDao;
 
-    public List<Achievement> getAchievementsForAll() {
-        return userAchievementsDao.getAchievementsForAllUser();
+    public List<UserAchievement> getAchievementsForAll() {
+        return userAchievementsDao.getAll();
     }
 
     public List<Achievement> getAchievementsByUserId(int userId) {
         return userAchievementsDao.getAchievementsByUserId(userId);
+    }
+
+    public void insertUserAchievements(List<UserAchievement> userAchievements) {
+        userAchievementsDao.insert(userAchievements);
+    }
+
+    public void deleteUserAchievements(List<UserAchievement> userAchievements) {
+        userAchievementsDao.delete(userAchievements.stream()
+                .map(UserAchievement::getId)
+                .collect(Collectors.toList()));
     }
 
 }
