88879704dff9caa74fee15069e4a349fc6672c57
AndrikB <blagij00@gmail.com>
2020-05-15 12:27:01 +0300

    add logs. fix bugs. move @EnableScheduling to QuizApplication

--- Quiz-server/src/main/java/com/qucat/quiz/repositories/entities/AchievementCondition.java
+++ Quiz-server/src/main/java/com/qucat/quiz/repositories/entities/AchievementCondition.java
@@ -3,15 +3,16 @@ package com.qucat.quiz.repositories.entities;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
 @Builder
 @AllArgsConstructor
 public class AchievementCondition {
-    private int id;
+    @EqualsAndHashCode.Exclude private int id;
     private ConditionOperator operator;
     private int value;
     private int achievementId;
     private int characteristicId;
-    private AchievementCharacteristic characteristic;
+    @EqualsAndHashCode.Exclude private AchievementCharacteristic characteristic;
 }
--- Quiz-server/src/main/java/com/qucat/quiz/services/SchedulerService.java
+++ Quiz-server/src/main/java/com/qucat/quiz/services/SchedulerService.java
@@ -2,12 +2,10 @@ package com.qucat.quiz.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 @Service
-@EnableScheduling
 @Slf4j
 public class SchedulerService {
 
@@ -18,8 +16,12 @@ public class SchedulerService {
     private TokenService tokenService;
 
     @Scheduled(cron = "0 0 * * * *")
-    public void reportCurrentTime() {
+    public void deleteOldTokens() {
         tokenService.deleteOldTokens();
+    }
+
+    @Scheduled(cron = "0 0 * * * *")
+    public void updateUserAchievementLists() {
         achievementService.updateUserAchievementLists();
     }
 
