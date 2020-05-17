e283dfff301427c07dd83548b32e3bd79e23ea05
Yevhenii Babiuk <evgeniy0199@gmail.com>
2020-05-17 20:14:50 +0300

    Delete redundant import

--- Quiz-server/src/main/java/com/qucat/quiz/repositories/dto/game/Users.java
+++ Quiz-server/src/main/java/com/qucat/quiz/repositories/dto/game/Users.java
@@ -1,7 +1,6 @@
 package com.qucat.quiz.repositories.dto.game;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.qucat.quiz.repositories.dto.game.UserDto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
