408c1686dbecbe3dfdd4f9fe807d401ed7d252ca
alexandrakmet <alexandrakmett@gmail.com>
2020-05-18 15:04:29 +0300

    add getMessageFromChat

--- Quiz-server/src/main/java/com/qucat/quiz/repositories/dao/MessageDao.java
+++ Quiz-server/src/main/java/com/qucat/quiz/repositories/dao/MessageDao.java
@@ -1,6 +1,8 @@
 package com.qucat.quiz.repositories.dao;
 
 import com.qucat.quiz.repositories.entities.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -8,4 +10,7 @@ public interface MessageDao extends GenericDao<Message> {
     String TABLE_NAME = "message";
 
     List<Message> getMessagesFromChat(int chatId);
+
+    Page<Message> getMessagesFromChat(int chatId, Pageable pageable);
+
 }
