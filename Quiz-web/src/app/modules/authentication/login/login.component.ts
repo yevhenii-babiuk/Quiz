44011a72c17ecad4063ac4ea408847eff8f6a5ae
Tetyana Steblevets <31775937+tnsts@users.noreply.github.com>
2020-05-20 09:00:01 +0300

    Chats localization (#110)
    * Chat pages added
    
    * Fix login & registration UI defects
    
    * Chats WebSockets
    
    * Chats server & client methods connection
    
    * Fix bugs
    
    * Chats work
    
    * Chat messages loading added
    
    * Chats & Login localization

--- Quiz-web/src/app/modules/authentication/login/login.component.ts
+++ Quiz-web/src/app/modules/authentication/login/login.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 
 import { AuthenticationService } from '../../core/services/authentication.service';
 import { AlertService } from '../../core/services/alert.service';
+import {TranslateService} from "@ngx-translate/core";
 
 import { User } from '../../core/models/user';
 
@@ -16,6 +17,7 @@ export class LoginComponent implements OnInit {
   constructor(
     private authenticationService: AuthenticationService,
     private alertService: AlertService,
+    public translate: TranslateService,
     private router: Router
   ) { }
 
