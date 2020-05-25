78d8ff84fdce648ce681ec6874c6dc6aa2671f21
Yevhenii Babiuk <evgeniy0199@gmail.com>
2020-05-25 21:12:22 +0300

    Delete unused import

--- Quiz-server/src/main/java/com/qucat/quiz/services/TagService.java
+++ Quiz-server/src/main/java/com/qucat/quiz/services/TagService.java
@@ -5,7 +5,6 @@ import com.qucat.quiz.repositories.entities.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
