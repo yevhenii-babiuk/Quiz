e4c4799689eef6ceafe54ae4b965abe934e674d5
AndrikB <blagij00@gmail.com>
2020-05-31 13:30:08 +0300

    add getLanguage on profile page

--- Quiz-web/src/app/modules/core/services/profile.service.ts
+++ Quiz-web/src/app/modules/core/services/profile.service.ts
@@ -5,6 +5,7 @@ import {User} from "../models/user";
 import {url} from "../../../../environments/environment.prod";
 import {catchError} from "rxjs/operators";
 import {Status} from "../models/Status";
+import {Lang} from "../models/lang";
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +21,10 @@ export class ProfileService {
 
   public getUser(id: number): Observable<User> {
     return this.http.get<User>(`${this.url}/users/${id}`);
+  }
+
+  public getLang(id: number): Observable<Lang> {
+    return this.http.get<Lang>(`${this.url}/users/${id}/getLang`);
   }
 
   public updateUser(user: User): Observable<User> {
