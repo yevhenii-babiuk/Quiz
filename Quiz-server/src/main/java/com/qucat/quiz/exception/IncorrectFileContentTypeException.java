babc2405fa6d60a0b9c3ffe6694b328159002f17
AndrikB <blagij00@gmail.com>
2020-06-01 18:49:12 +0300

    add check contentType of file

---
+++ Quiz-server/src/main/java/com/qucat/quiz/exception/IncorrectFileContentTypeException.java
@@ -0,0 +1,7 @@
+package com.qucat.quiz.exception;
+
+public class IncorrectFileContentTypeException extends RuntimeException {
+    public IncorrectFileContentTypeException(String message) {
+        super(message);
+    }
+}
